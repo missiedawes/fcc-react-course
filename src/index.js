20323f2c1650440a27d9ae8f2c5d47f282e4b8c8
Missie Becker <Missie@Missies-MacBook-Air.local>
2022-04-23 18:39:34 -0400

    add project to the cyberworld

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const page = (
+  <div>
+    <h1>My awesome website in React</h1>
+    <h3>Reasons I should love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(page);
