90463d13ee6979da8a777104b55f785ae012da08
missiedawes <missiedawes@gmail.com>
2022-04-23 19:38:33 -0400

    create project one's page with unstyled content

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./React-icon.png";
 
 const page = (
   <div>
-    <h1>My awesome website in React</h1>
-    <h3>Reasons I should love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>It's a hireable skill</li>
-      <li>It's actively maintained by skilled people</li>
-    </ol>
+    <img src={logo} alt="react logo"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K star on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(page);
