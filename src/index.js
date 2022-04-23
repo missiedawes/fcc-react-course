31da50ef4a5f9f3e58f4b2095765ae1be63642b8
missiedawes <missiedawes@gmail.com>
2022-04-23 19:38:04 -0400

    Create my first react page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reactIcon from './React-icon.svg';
 
 const page = (
   <div>
-    <h1>My awesome website in React</h1>
-    <h3>Reasons I should love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>It's a hireable skill</li>
-      <li>It's actively maintained by skilled people</li>
-    </ol>
+
+  <div>
+    <img src={reactIcon} alt=""></img>
+  </div>
+  <div>
+    <div>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I should love React</h3>
+      <ol>
+        <li>It's composable</li>
+        <li>It's declarative</li>
+        <li>It's a hireable skill</li>
+        <li>It's actively maintained by skilled people</li>
+      </ol>
+    </div>
+  </div>
   </div>
 )
 
